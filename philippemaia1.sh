d61652b991acf83d2ae69c81276772a96c4b26ce
Philippe Maia <philippe.maia@stone.com.br>
2018-06-22 17:11:13 -0300

    Update philippemaia1.sh

--- philippemaia1.sh
+++ philippemaia1.sh
@@ -1,7 +1,7 @@
 
 #!bin/sh
 
-git config --global user.name "Philippe maia"
+git config --global user.name "philippe maia"
 git config --global user.email "phil.maia@gmail.com"
 git add .
 git commit -m "Trab LAB SO"
